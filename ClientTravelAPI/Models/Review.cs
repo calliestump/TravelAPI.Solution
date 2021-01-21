0c695ef8ef6aac7bfb70b37473b7f9b354da5d3c
Austin Schrader <austin.jschrader@gmail.com>
2021-01-21 14:51:59 -0800

    Add review model

--- ClientTravelAPI/Models/Review.cs
+++ ClientTravelAPI/Models/Review.cs
@@ -1,5 +1,25 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ClientTravelAPI.Models
 {
+  public class Review
+  {
+    public int ReviewId { get; set; }
+    public int Rating { get; set; }
+    public string Description { get; set; }
 
+    public static List<Review> GetReviews()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Review> reviewList = JsonConvert.DeserializeObject<List<Review>>(jsonResponse.ToString());
+
+      return reviewList;
+    }
+  }
 }
