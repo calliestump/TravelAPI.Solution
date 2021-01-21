dc587f5b2a27ca5602efaa5b1a2dfb18deb6a825
Austin Schrader <austin.jschrader@gmail.com>
2021-01-21 11:33:28 -0800

    Add api helper class to program
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- ClientTravelAPI/Program.cs
+++ ClientTravelAPI/Program.cs
@@ -7,18 +7,33 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace ClientTravelAPI
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+      var apiCallTask = ApiHelper.ApiCall();
+      var result = apiCallTask.Result;
+      Console.WriteLine(result);
+      CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+  }
+
+  public class ApiHelper
+  {
+    public static async Task<string> ApiCall()
+    {
+      RestClient client = new RestClient("https://localhost:5000/api");
+      RestRequest request = new RestRequest($"home.json", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
 }
